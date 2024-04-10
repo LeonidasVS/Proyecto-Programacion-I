32e0420137ddc40e828c14f5dad70a6d48799f86
LeonidasVs <villafrancosibrian@gmail.com>
2024-04-10 13:25:33 -0600

    Cambios

--- Capa_Entidades/Venta.cs
+++ Capa_Entidades/Venta.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Capa_Entidades
 {
-    public class Venta
+    public class Ventas
     {
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
