516a77e2b4135cae8fbc438e2025cfaebc24a512
LeonidasVs <villafrancosibrian@gmail.com>
2024-04-24 10:15:18 -0600

    Cambios

--- CapaDatos/VentaDAL.cs
+++ CapaDatos/VentaDAL.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Capa_Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,42 @@ namespace CapaDatos
 {
     public class VentaDAL
     {
+        ContextoBD contexto;
+
+        public int GuardarVenta(Venta venta, int id = 0, bool actualizacion = false)
+        {
+            contexto = new ContextoBD();
+            int resultado;
+
+            if (actualizacion)
+            {
+                venta.idVenta = id;
+
+                contexto.Entry(venta).State = System.Data.Entity.EntityState.Modified;
+                contexto.SaveChanges();
+
+                resultado = venta.idVenta;
+            }
+            else
+            {
+                contexto.Venta.Add(venta);
+                contexto.SaveChanges();
+
+                resultado = venta.idVenta;
+            }
+
+            return resultado;
+        }
+
+        public List<Venta> LeerVenta()
+        {
+            contexto = new ContextoBD();
+            return contexto.Venta.Where(p => p.idVenta>0 == true).ToList();
+        }
+        public Venta ObtenerVenta(int id)
+        {
+            contexto = new ContextoBD();
+            return contexto.Venta.Find(id);
+        }
     }
 }
